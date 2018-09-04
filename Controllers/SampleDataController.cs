3fda52f5be72e279426541c92f25a20dd1523969
vvvlad <vvvlad42@gmail.com>
2018-09-04 20:24:29 +0300

    added SQL connectivity

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -3,27 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RCheetah.Dal;
 
 namespace RCheetah.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public SampleDataController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         private static string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+        
 
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts()
         {
+
+            SqlDal sql = new SqlDal(configuration);
+            var testValue = sql.TestSql();
+
+            //var exampleValueFromConfig = configuration["Greeting"];
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+            var toReturn = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             });
+
+            var testW = new WeatherForecast { DateFormatted = DateTime.Now.ToString("d"), TemperatureC = 0, Summary = testValue };
+            toReturn =toReturn.ToList().Append(testW);
+            
+            return toReturn;
         }
 
         public class WeatherForecast
---
+++ Dal/SqlDal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace RCheetah.Dal
+{
+    public class SqlDal
+    {
+        private readonly IConfiguration _configuration;
+        public SqlDal(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string TestSql()
+        {
+            SqlConnectionStringBuilder con = new SqlConnectionStringBuilder();
+            var connString = _configuration["ConnectionStrings:DefaultConnection"];
+
+            string queryString = "SELECT test FROM [dbo].[Table] WHERE id=1";
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = queryString;
+                connection.Open();
+
+                using(SqlDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        var x = reader["test"].ToString();
+                        return x;
+                    }
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
